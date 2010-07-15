201748a435c2c9d085a5e31430322d40dd22fb19
Justin Weaver <jiweaver@users.noreply.github.com>
2010-07-15 00:31:40 +0000

    Fix the license.

--- QuickLinkAPI4NET/QuickLinkAPI4NET/QuickLink.cs
+++ QuickLinkAPI4NET/QuickLinkAPI4NET/QuickLink.cs
@@ -1,4 +1,27 @@
-﻿/* File: QuickLink.cs
+﻿/* QuickLinkAPI4NET : A .NET wrapper (in C#) for EyeTech's QuickLink API.
+ *
+ * Copyright (c) 2010 Justin Weaver
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/* File: QuickLink.cs
  * Author: Justin Weaver (Jul 2010)
  * Description: A .NET wrapper for EyeTech's QuickLink API. The extensive
  * inline documentation (i.e. comments) has been cut & pasted from the 
